c169ce2229c792ec81ae9763c206f98004bf2fcb
QiangRO <aroch22222@hotmail.com>
2022-09-08 09:27:04 -0400

    Se agrega el entrypoint de las interfaces

--- src/main/kotlin/POO/Interfaces/Main.kt
+++ src/main/kotlin/POO/Interfaces/Main.kt
@@ -1,5 +1,8 @@
 package POO.Interfaces
 
 fun main(){
-
+    val vehiculo = Vehiculo()
+    println(vehiculo.calculatePositionGps())
+    println(vehiculo.showInformation())
+    println(vehiculo.enableMovement())
 }
