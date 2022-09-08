45f4c34c88fc47246370b3d42f691081884d5eaf
QiangRO <aroch22222@hotmail.com>
2022-09-08 09:29:42 -0400

    Se agrega la clase vehiculo que implementa las interfaces localizable y security

--- src/main/kotlin/POO/Interfaces/Vehiculo.kt
+++ src/main/kotlin/POO/Interfaces/Vehiculo.kt
@@ -1,14 +1,27 @@
 package POO.Interfaces
 /* Implementar interfaz usando operador: */
-class Vehiculo: Localizable {
-    override var latitud: Double
+class Vehiculo: Localizable, Security {
+    // Localizable Interfaz
+    override var latitud: Double = 0.0
+    override var longitud: Double = 0.0
+    override var redButton: Boolean
         get() = TODO("Not yet implemented")
         set(value) {}
-    override var longitud: Double
-        get() = TODO("Not yet implemented")
-        set(value) {}
+    private var velocity: Double = 0.0
 
     override fun calculatePositionGps(): String {
-        TODO("Not yet implemented")
+        latitud = (-20..-10).random().toDouble()
+        longitud = (-80..-60).random().toDouble()
+        return "{$latitud - $longitud}"
+    }
+
+    override fun showInformation(): String {
+        return super.showInformation()
+    }
+
+    override fun enableMovement() {
+        velocity = if(!redButton)(10..20).random().toDouble()
+        else 0.0
+        val aceleracion = 1.0
     }
 }
