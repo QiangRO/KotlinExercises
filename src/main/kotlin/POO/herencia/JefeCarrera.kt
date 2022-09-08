d3889a193ce2b87a14caf42fe105585804feefb4
QiangRO <aroch22222@hotmail.com>
2022-09-08 09:29:49 -0400

    --

--- src/main/kotlin/POO/herencia/JefeCarrera.kt
+++ src/main/kotlin/POO/herencia/JefeCarrera.kt
@@ -8,35 +8,4 @@ package POO.herencia
 //    un numero aleatrorio entre 1000 y 5000Bs
 //5) buscar una manera de calcular el salario
 //6)la sancion y el bono deben afectar directamente al salario ganado
-class JefeCarrera(
-    nombre: String,
-    apellido: String,
-    experiencia: Int,
-    area: String,
-    salario: Int,
-): persona(nombre,apellido,experiencia,area,salario ) {
-    //Atributos
-    var carreara:String =""
-    init {
-        this.bono=calcularBono()
-        this.salario= this.bono
-    }
-
-
-
-    //Metodos
-    override fun mostrarInformacion(): String {
-        return super.mostrarInformacion()
-        println("Es jefe de la carrera de $carreara")
-
-    }
-
-    override fun calcularBono(): Int {
-        return this.salario + (this.salario*15/100)+ rand()
-    }
-
-    fun rand(start: Int= 1000, end: Int=5000): Int {
-        require(!(start > end || end - start + 1 > Int.MAX_VALUE)) { "Illegal Argument" }
-        return Random(System.nanoTime()).nextInt(end - start + 1) + start
-    }
-}
+class JefeCarrera{}
